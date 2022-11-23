277404f246583ea25d7ca6af16311f837b846813
unknown <ulanovnikolay10p@gmail.com>
2022-11-23 20:21:14 +0300

    with start of redux toolkite

---
+++ cloneReduxToolkitPractice/src/store/index.js
@@ -0,0 +1,38 @@
+
+import { configureStore } from "@reduxjs/toolkit";
+import heroes from '../reducers/heroes';
+import filters from '../reducers/filters';
+
+
+const stringMiddleware = (store) => (next) => (action) => {//Делает то же самое, что и энхенсер, но работает напрямую с диспетчем а не сос тором
+    if (typeof action === "string") {
+        return next({
+            type: action
+        })
+    }
+    return next(action)
+}
+
+
+const store = configureStore({
+    reducer: {heroes, filters},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),//Уже включены три мидлвеер
+    devTools: process.env.NODE_ENV !== "production",
+})
+
+
+// const store = createStore(
+//     combineReducers({heroes, filters}),
+//     // compose(
+//     //     enhancer,
+//     //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     // )
+//     compose(
+//         applyMiddleware(ReduxThunk, stringMiddleware),//Должно работать также как с энхенсером
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+
+// )
+export default store;
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
